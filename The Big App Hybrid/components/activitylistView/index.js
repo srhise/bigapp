41bf3845685543b245d620da59f791b2a68fafdd
Sean Wilkening <sean@ia-nyc.com>
2015-08-10 19:25:38 +0000

    Changes from Screen Builder

--- The Big App Hybrid/components/activitylistView/index.js
+++ The Big App Hybrid/components/activitylistView/index.js
@@ -27,7 +27,19 @@ app.activitylistView = kendo.observable({
         },
         dataSource = new kendo.data.DataSource(dataSourceOptions),
         activitylistViewModel = kendo.observable({
-            dataSource: dataSource
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/activitylistView/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    itemModel = dataSource.getByUid(item);
+                if (!itemModel.text) {
+                    itemModel.text = String.fromCharCode(160);
+                }
+                activitylistViewModel.set('currentItem', itemModel);
+            },
+            currentItem: null
         });
 
     parent.set('activitylistViewModel', activitylistViewModel);
